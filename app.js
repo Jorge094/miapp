ec32ee2df843abe61fb0ae948c1177e07acc7cd6
Jorge094 <andres.lllanquin@gmail.com>
2025-07-15 13:15:51 -0400

    seq ID and delete

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const formulario= document.querySelector(".form-create");
+let contador = 1;
 formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML += `<div class="publicacion">
-            <p>id: 00</p>
+    contenedor.innerHTML += `<div id="${contador}"class="publicacion">
+            <p>id: ${contador}p>
             <p>${descripcion}</p>
-            <button>Editar</button>
-            <button>Eliminar</button>
+            <button id="editar" onclick="editar(this)">Editar</button>
+            <button id="eliminar" onclick="eliminar(this)">Eliminar</button>
         </div>`;
+        contador++;
 });
+
+function editar(e){
+
+}
+
+function eliminar(e){
+    e.parentElement.remove(); //sirve para poder borrar la publicación que deseas
+}
